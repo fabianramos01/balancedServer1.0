41f8a7505c74675187ae20f499ce36f8c16a4d61
Fabián N. Ramos C <36746364+fabianramos01@users.noreply.github.com>
2019-05-04 22:14:54 -0500

    Create service

---
+++ service.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+# This service requires you have forever app installed in your system.
+ 
+ 
+### CONFIG ZONE
+# This is the service name. It cant have any special char or space.
+serviceName="server3015"
+ 
+# Where is located your app
+appFilePath="/balancedServer1.0"
+ 
+# Whats the name of your main app file.
+appIndex="server.js"
+ 
+enviroment=production
+### END CONFIG ZONE
+ 
+ 
+###### DO NOT EDIT BELOW THIS LINE ######
+# Source function library.
+. /lib/lsb/init-functions
+ 
+pidFile="/var/run/$serviceName.pid"
+logFile="/var/run/$serviceName.log"
+ 
+command="node"
+nodeApp="$appFilePath/$appIndex"
+foreverApp="forever"
+ 
+start() {
+	echo "Starting $serviceName"
+	PATH=/usr/local/bin:$PATH
+	export NODE_ENV=$enviroment
+	$foreverApp start --pidFile $pidFile -l $logFile -a -d -c "$command" $nodeApp
+	RETVAL=$?
+}
+ 
+restart() {
+	echo -n "Restarting $serviceName"
+	$foreverApp restart $nodeApp
+	RETVAL=$?
+}
+ 
+stop() {
+	echo -n "Shutting down $serviceName"
+	$foreverApp stop $nodeApp
+	RETVAL=$?
+}
+ 
+status() {
+   echo -n "Status $serviceName"
+   $foreverApp list
+   RETVAL=$?
+}
+ 
+case "$1" in
+   start)
+        start
+        ;;
+    stop)
+        stop
+        ;;
+   status)
+        status
+       ;;
+   restart)
+   	restart
+        ;;
+	*)
+       echo "Usage:  {start|stop|status|restart}"
+       exit 1
+        ;;
+esac
+exit $RETVAL
